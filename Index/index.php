cc84b02cee8486e60c9b56c30aff88607e950ffd
Ismael Bodas <ismaelbd042@gmail.com>
2024-04-19 11:35:36 +0200

    Creado el header y estructura de includes. Header no terminado

---
+++ Index/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../header y footer/style.css">
+    <title>Topanga</title>
+</head>
+
+<body>
+    <?php
+    include "../header y footer/header.html"
+    ?>
+    <!-- Aquí iría todo el código del index -->
+    <?php
+    // include "../header y footer/footer.html"
+    ?>
+</body>
+
+</html>
