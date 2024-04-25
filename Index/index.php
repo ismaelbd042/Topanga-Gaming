2f3640cacb9209668bfeb9d28112b1c5be0c9aa6
Ismael Bodas Díaz <ismaelbd042@gmail.com>
2024-04-25 11:42:34 +0200

    Cambios en estilos de la pagina, ventana modal funcional (se abre y se cierra) y arreglos en la base de datos

--- Index/index.php
+++ Index/index.php
@@ -4,21 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../header y footer/style.css">
+    <!-- <link rel="stylesheet" href="../header y footer/style.css"> -->
+    <link rel="stylesheet" href="style.css">
     <title>Topanga</title>
 </head>
 
 <body>
+    <div class="overlay"></div>
     <?php
     include "../header y footer/header.html";
+    include "../header y footer/VentanaModal.html";
     include "../database/conect.php";
     ?>
-    <!-- Aquí iría todo el código del index -->
+    <div class="containerIndex">
+        <!-- Aquí iría todo el código del index -->
+    </div>
     <!-- <span onclick="playSound('https://zero-network.net/phasmophobia/static/assets/banshee_scream.mp3')">Icono de sonido</span> -->
     <!-- codigo que hace que suene un sonido -->
     <?php
     // include "../header y footer/footer.html"
     ?>
+    <script src="script.js"></script>
 </body>
 
 </html>
---
+++ Index/script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+  mostrarVentanaModal();
+});
+
+function mostrarVentanaModal() {
+  // Obtener la ventana modal y el checkbox
+  let modal = document.querySelector(".mainVentanaModal");
+  let chkVentanaModal = document.getElementById("chkVentanaModal");
+  let overlay = document.querySelector(".overlay");
+
+  // Obtener los botones de inicio de sesión y registro
+  let btnIniciarSesion = document.querySelector(".iniciarSesion");
+  let btnRegistrarse = document.querySelector(".registrarse");
+
+  // Función para cambiar la ventana modal al estado de inicio de sesión
+  function mostrarLogin() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+    setTimeout(function () {
+      modal.style.transform = "translate(-50%, -50%)"; // Cambiar la transformación para deslizarse desde abajo
+    }, 100); // Pequeña demora antes de aplicar la transformación
+    chkVentanaModal.checked = true;
+  }
+
+  // Función para cambiar la ventana modal al estado de registro
+  function mostrarSignUp() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+    setTimeout(function () {
+      modal.style.transform = "translate(-50%, -50%)"; // Cambiar la transformación para deslizarse desde abajo
+    }, 100); // Pequeña demora antes de aplicar la transformación
+    chkVentanaModal.checked = false;
+  }
+
+  // Función para cerrar la ventana modal
+  function ocultarVentanaModal() {
+    modal.style.transform = "translate(-50%, 100%)";
+    setTimeout(function () {
+      modal.style.display = "none";
+    }, 500);
+    overlay.style.display = "none";
+  }
+
+  // Agregar eventos de clic a los botones
+  btnIniciarSesion.addEventListener("click", mostrarLogin);
+  btnRegistrarse.addEventListener("click", mostrarSignUp);
+  overlay.addEventListener("click", ocultarVentanaModal);
+}
+
+// function ocultarVentanaModal() {
+//   modal.style.transform = "translate(-50%, 100%)";
+//   setTimeout(function () {
+//     modal.style.display = "none";
+//   }, 500);
+//   overlay.style.display = "none";
+// }
