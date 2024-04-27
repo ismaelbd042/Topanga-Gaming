cf2f3bc5f2112210e5ba306ab953e8aa95f0a7f6
Ismael Bodas <ismaelbd042@gmail.com>
2024-04-27 16:12:38 +0200

    Arreglo del ojo y eliminación de código inecesario

--- Index/script.js
+++ Index/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   mostrarVentanaModal();
-  // validarFormulariosAcceso();
+  validarFormulariosAcceso();
 });
 
 function mostrarVentanaModal() {
@@ -49,20 +49,12 @@ function mostrarVentanaModal() {
 }
 
 function validarFormulariosAcceso() {
-  // Obtención de elementos del formulario de inicio de sesión y registro
+  // Obtención de elementos del formulario de inicio de sesión
   let loginForm = document.getElementById("login");
-  let registerForm = document.getElementById("registrar");
 
   // Validación del formulario de inicio de sesión al enviar
   loginForm.addEventListener("submit", function (event) {
     if (!validateLoginForm()) {
-      event.preventDefault();
-    }
-  });
-
-  // Validación del formulario de registro al enviar
-  registerForm.addEventListener("submit", function (event) {
-    if (!validateRegisterForm()) {
       event.preventDefault();
     }
   });
@@ -109,85 +101,6 @@ function validarFormulariosAcceso() {
     // Mensaje de error
     errorMessage.style.display = "none";
     return true;
-  }
-
-  // Función para validar el formulario de registro
-  function validateRegisterForm() {
-    let username = registerForm.querySelector("input[type='text']").value;
-    let email = registerForm.querySelector("input[type='email']").value;
-    let password = registerForm.querySelector("input[type='password']").value;
-    let errorMessage = registerForm.querySelector(".error-message-registro");
-
-    let usernameErrorIcon = document.querySelector(".username-error");
-    let emailErrorIcon = document.querySelector(".email-error");
-    let passwordErrorIcon = document.querySelector(".password-error");
-
-    // If para comprobar si el usuario esta vacio
-    if (username.trim() === "") {
-      errorMessage.textContent = "Por favor, completa todos los campos.";
-      errorMessage.style.display = "block";
-      usernameErrorIcon.style.display = "block";
-      return false;
-    } else {
-      usernameErrorIcon.style.display = "none";
-    }
-    // If para comprobar si el email esta vacio
-    if (email.trim() === "") {
-      errorMessage.textContent = "Por favor, completa todos los campos.";
-      errorMessage.style.display = "block";
-      emailErrorIcon.style.display = "block";
-      return false;
-    } else {
-      emailErrorIcon.style.display = "none";
-    }
-
-    // If para comporbar si la contyraseña esta vacia
-    if (password.trim() === "") {
-      errorMessage.textContent = "Por favor, completa todos los campos.";
-      errorMessage.style.display = "block";
-      passwordErrorIcon.style.display = "block";
-      return false;
-    } else {
-      passwordErrorIcon.style.display = "none";
-    }
-
-    // Verificación por si el email no es valido
-    if (!validateEmail(email)) {
-      errorMessage.textContent =
-        "Por favor, introduce un correo electrónico válido.";
-      errorMessage.style.display = "block";
-      emailErrorIcon.style.display = "block";
-      return false;
-    } else {
-      emailErrorIcon.style.display = "none";
-    }
-
-    // Formulario para validar la contraseña, tiene que tener los requerimientos siguientes en la funcion validate
-    if (!validatePassword(password)) {
-      errorMessage.textContent =
-        "La contraseña debe tener al menos 8 caracteres, incluyendo una mayúscula, una minúscula y un número.";
-      errorMessage.style.display = "block";
-      passwordErrorIcon.style.display = "block";
-      return false;
-    } else {
-      passwordErrorIcon.style.display = "none";
-    }
-
-    // Mensaje de error
-    errorMessage.style.display = "none";
-    return true;
-  }
-
-  // Función para validar la fortaleza de la contraseña
-  function validatePassword(password) {
-    let minLength = 8;
-    let hasUpperCase = /[A-Z]/.test(password);
-    let hasLowerCase = /[a-z]/.test(password);
-    let hasNumbers = /\d/.test(password);
-
-    return (
-      password.length >= minLength && hasUpperCase && hasLowerCase && hasNumbers
-    );
   }
 
   // Función para validar un correo electrónico con un patrón regex
