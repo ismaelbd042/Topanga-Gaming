a8088051269c57582f7f44f4b6c5bc275f49855d
Ismael Bodas Díaz <ismaelbd042@gmail.com>
2024-05-07 13:01:29 +0200

    Añadimos fotos de modelos de fantasmas

--- fantasmas/fantasmas.php
+++ fantasmas/fantasmas.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,6 +25,7 @@
             display: flex;
             flex-direction: row;
             margin: 2%;
+            overflow: hidden;
         }
 
         .cuadrado_morado {
@@ -31,6 +33,8 @@
             height: 100%;
             clip-path: polygon(60% 0, 100% 50%, 60% 100%, 0 100%, 0 0);
             background: radial-gradient(185.32% 99.8% at 11.32% 52.18%, #003 0%, #5F1495 100%);
+            border-top-left-radius: 30px;
+            border-bottom-left-radius: 30px;
         }
 
         .cuadrado_morado img {
@@ -80,47 +84,49 @@
         }
     </style>
 </head>
+
 <body>
-<?php
-include "../database/connect.php";
+    <?php
+    include "../database/connect.php";
 
-// Obtener la conexión a la base de datos
-$conexion = getConexion();
+    // Obtener la conexión a la base de datos
+    $conexion = getConexion();
 
-// Consulta para obtener los datos de los fantasmas y sus pruebas asociadas
-$sql = "SELECT f.nombre AS nombre_fantasma, f.descripcion AS descripcion_fantasma, 
-GROUP_CONCAT(p.nombre) AS pruebas
-FROM fantasmas f 
-INNER JOIN pruebas_fantasmas pf ON f.id = pf.fantasma_id
-INNER JOIN pruebas p ON pf.prueba_id = p.id
-GROUP BY f.id";
-$result = mysqli_query($conexion, $sql);
+    // Consulta para obtener los datos de los fantasmas y sus pruebas asociadas
+    $sql = "SELECT f.nombre AS nombre_fantasma, f.descripcion AS descripcion_fantasma, 
+        GROUP_CONCAT(p.nombre) AS pruebas
+        FROM fantasmas f 
+        INNER JOIN pruebas_fantasmas pf ON f.id = pf.fantasma_id
+        INNER JOIN pruebas p ON pf.prueba_id = p.id
+        GROUP BY f.id";
+    $result = mysqli_query($conexion, $sql);
 
-echo '<div class="granTarjeta">';
-if (mysqli_num_rows($result) > 0) {
-// Generar las tarjetas HTML para cada fantasma
-while($row = mysqli_fetch_assoc($result)) {
-echo '<div class="tarjeta_fantasma_general">';
-echo '<div class="cuadrado_morado"><img src="../img/Fotos fantasmas/'.strtolower($row["nombre_fantasma"]).'.svg"></img></div>';
-echo '<div class="info_fantasma">';
-echo '<span class="nombre_fantasma">' . $row["nombre_fantasma"] . '</span>';
-echo '<div class="desc_fantasma">' . nl2br($row["descripcion_fantasma"]) . '</div>';
-echo '<div class="pruebas_fantasmas">';
+    echo '<div class="granTarjeta">';
+    if (mysqli_num_rows($result) > 0) {
+        // Generar las tarjetas HTML para cada fantasma
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<div class="tarjeta_fantasma_general">';
+            echo '<div class="cuadrado_morado"><img src="../img/Fotos fantasmas/' . strtolower($row["nombre_fantasma"]) . '.svg"></img></div>';
+            echo '<div class="info_fantasma">';
+            echo '<span class="nombre_fantasma">' . $row["nombre_fantasma"] . '</span>';
+            echo '<div class="desc_fantasma">' . nl2br($row["descripcion_fantasma"]) . '</div>';
+            echo '<div class="pruebas_fantasmas">';
 
-// Obtener y mostrar las pruebas asociadas al fantasma
-$pruebas = explode(",", $row["pruebas"]);
-foreach ($pruebas as $key => $prueba) {
-echo '<div class="pruebas prueba' . ($key + 1) . '"><img src="../img/Fotos pruebas/'.$prueba.'.svg" alt="">' . $prueba . '</div>';
-}
+            // Obtener y mostrar las pruebas asociadas al fantasma
+            $pruebas = explode(",", $row["pruebas"]);
+            foreach ($pruebas as $key => $prueba) {
+                echo '<div class="pruebas prueba' . ($key + 1) . '"><img src="../img/Fotos pruebas/' . $prueba . '.svg" alt="">' . $prueba . '</div>';
+            }
 
-echo '</div></div></div>';
-}
-} else {
-echo "0 resultados";
-}
-echo '</div>';
-// Cerrar la conexión a la base de datos
-mysqli_close($conexion);
-?>
+            echo '</div></div></div>';
+        }
+    } else {
+        echo "0 resultados";
+    }
+    echo '</div>';
+    // Cerrar la conexión a la base de datos
+    mysqli_close($conexion);
+    ?>
 </body>
+
 </html>
