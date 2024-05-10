9ced9aea55d90d71a8a3d9030467eb1e7b69d8c2
lucca23 <luccamanfredotti@gmail.com>
2024-05-10 18:03:34 +0200

    Realizamos la pagina fantasmas con el buscador de filtro por pruebas

--- fantasmas/fantasmas.php
+++ fantasmas/fantasmas.php
@@ -113,7 +113,6 @@
             /* Reducir el margen */
             display: flex;
             text-align: center;
-            
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         }
     </style>
