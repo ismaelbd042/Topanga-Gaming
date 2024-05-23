704b7e7458c7483702d445ce6ad78a9c820a8527
Ismael Bodas <ismaelbd042@gmail.com>
2024-05-23 14:55:55 +0200

    Arreglamos el link al chat

--- Index/script.js
+++ Index/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   mostrarVentanaModal();
+  comprobarSesion();
   validarFormulariosAcceso();
   validadSesion();
 });
@@ -132,4 +133,68 @@ function validadSesion() {
       }
     })
     .catch((error) => console.error("Error:", error));
+}
+
+function comprobarSesion() {
+  fetch("../php_acceso/verificar_sesion.php")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.sesion_activa) {
+        document.getElementById("enlace_chat").href =
+          "../chat_mensajes/chat.php";
+      } else {
+        document
+          .getElementById("enlace_chat")
+          .addEventListener("click", function (event) {
+            event.preventDefault();
+            mostrarVentanaModal();
+            mostrarLogin();
+          });
+      }
+    })
+    .catch((error) => {
+      console.error("Error al verificar la sesión:", error);
+    });
+  // Obtener la ventana modal y el checkbox
+  let modal = document.querySelector(".mainVentanaModal");
+  let chkVentanaModal = document.getElementById("chkVentanaModal");
+  let overlay = document.querySelector(".overlay");
+
+  // Obtener los botones de inicio de sesión y registro
+  let btnIniciarSesion = document.querySelector(".iniciarSesion");
+  let btnRegistrarse = document.querySelector(".registrarse");
+
+  // Función para cambiar la ventana modal al estado de inicio de sesión
+  function mostrarLogin() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+    setTimeout(function () {
+      modal.style.transform = "translate(-50%, -50%)"; // Cambiar la transformación para deslizarse desde abajo
+    }, 100); // Pequeña demora antes de aplicar la transformación
+    chkVentanaModal.checked = true;
+  }
+
+  // Función para cambiar la ventana modal al estado de registro
+  function mostrarSignUp() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+    setTimeout(function () {
+      modal.style.transform = "translate(-50%, -50%)"; // Cambiar la transformación para deslizarse desde abajo
+    }, 100); // Pequeña demora antes de aplicar la transformación
+    chkVentanaModal.checked = false;
+  }
+
+  // Función para cerrar la ventana modal
+  function ocultarVentanaModal() {
+    modal.style.transform = "translate(-50%, 100%)";
+    setTimeout(function () {
+      modal.style.display = "none";
+    }, 500);
+    overlay.style.display = "none";
+  }
+
+  // Agregar eventos de clic a los botones
+  btnIniciarSesion.addEventListener("click", mostrarLogin);
+  btnRegistrarse.addEventListener("click", mostrarSignUp);
+  overlay.addEventListener("click", ocultarVentanaModal);
 }
