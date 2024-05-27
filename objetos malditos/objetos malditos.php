f8eb4341b6b7a0fa4bd86f17efd55c3fedf2859a
alvaro <alvaroserrano0109@gmail.com>
2024-05-27 12:33:32 +0200

    Tarjeta fantamas pruebas, objetos malditos ajustado, im iconoes nuevos

--- objetos malditos/objetos malditos.php
+++ objetos malditos/objetos malditos.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../img/Logo fondo blanco.svg" type="image/x-icon">
+    <link rel="stylesheet" href="../Index/style.css">
     <title>Topanga Gaming</title>
     <style>
         @font-face {
