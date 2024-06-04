8c1978773f95c3577da34883b2697565c06fdcfe
alvaro <alvaroserrano0109@gmail.com>
2024-06-04 11:05:01 +0200

    Actualizo el index, Gafton Farmhouse y mapas

--- Index/index.php
+++ Index/index.php
@@ -25,62 +25,73 @@
         <div class="Index2">
             <div class="tituloMapa">Investiga los Mapas</div>
             <div class="corchoMapas">
-                <div class="tarjeta_mapa_corcho" id="Ridgeview">
+                <div class="tarjeta_mapa_corcho" id="Ridgeview" data-mapa="10_Ridgeview_Court">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/10_Ridgeview_Court.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">10 Ridgeview Court</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Willow">
+                <div class="tarjeta_mapa_corcho" id="Willow" data-mapa="13_Willow_Street">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/13_Willow_Street.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">13 Willow Street</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Edgefield">
+                <div class="tarjeta_mapa_corcho" id="Edgefield" data-mapa="42_Edgefield_Road">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/42_Edgefield_Road.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">42 Edgefield Road</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Tanglewood">
+                <div class="tarjeta_mapa_corcho" id="Tanglewood" data-mapa="6_Tanglewood_Drive">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/6_Tanglewood_Drive.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">6 Tanglewood drive</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Bleasdale">
+                <div class="tarjeta_mapa_corcho" id="Bleasdale" data-mapa="Bleasdale_Farmhouse">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Bleasdale_Farmhouse.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Bleasdale Farmhouse</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="BrownStone">
+                <div class="tarjeta_mapa_corcho" id="BrownStone" data-mapa="Brownstone_High_School">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Brownstone_High_School.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Brownstone High School</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Woodwind">
+                <div class="tarjeta_mapa_corcho" id="Woodwind" data-mapa="Camp_Woodwind">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Camp_Woodwind.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Camp Woodwind</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Farmhouse">
+                <div class="tarjeta_mapa_corcho" id="Farmhouse" data-mapa="Grafton_Farmhouse">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Grafton_Farmhouse.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Grafton Farmhouse</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Lodge">
+                <div class="tarjeta_mapa_corcho" id="Lodge" data-mapa="Maple_Lodge_Campsite">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Maple_Lodge_Campsite.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Maple Lodge Campsite</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Prison">
+                <div class="tarjeta_mapa_corcho" id="Prison" data-mapa="Prison">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Prison.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Prision</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Meadows">
+                <div class="tarjeta_mapa_corcho" id="Meadows" data-mapa="Sunny_Meadows_Mental_Institution">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Sunny_Meadows_Mental_Institution.svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Sunny Meadows</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="MeadowsRestricted">
+                <div class="tarjeta_mapa_corcho" id="MeadowsRestricted" data-mapa="Sunny_Meadows_Mental_Institution_(Restricted)">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Sunny_Meadows_Mental_Institution_(Restricted).svg" alt=""></div>
                     <span class="nombre_mapa_corcho">Sunny Meadows Restricted</span>
                 </div>
-                <div class="tarjeta_mapa_corcho" id="Azar">
+                <div class="tarjeta_mapa_corcho" id="Azar" data-mapa="Azar">
                     <div class="imgsMapas"><img src="../img/Fotos mapas/Azar.png" alt=""></div>
                     <span class="nombre_mapa_corcho">Al azar</span>
                 </div>
             </div>
         </div>
-
     </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const tarjetasMapa = document.querySelectorAll(".tarjeta_mapa_corcho");
+    
+            tarjetasMapa.forEach(tarjeta => {
+                tarjeta.addEventListener("click", function() {
+                    const nombreMapa = this.getAttribute("data-mapa");
+                    window.location.href = `../mapas/mapas.php?mapa=${nombreMapa}`;
+                });
+            });
+        });
+    </script>
 
 
 
