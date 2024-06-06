4b4de0729e4190ac21dce970ead1ff9b8fb85873
Ismael Bodas Díaz <ismaelbd042@gmail.com>
2024-06-06 13:11:03 +0200

    Cambios en chat y buscar amigos

--- chat_mensajes/buscar_usuario.php
+++ chat_mensajes/buscar_usuario.php
@@ -9,22 +9,22 @@ $busqueda = $conn->real_escape_string($busqueda);
 $sql = "SELECT id, nombre_usuario, correo 
         FROM usuarios 
         WHERE nombre_usuario LIKE '%$busqueda%' 
-          AND id NOT IN (
-              SELECT amigo_id 
-              FROM amigos 
-              WHERE usuario_id = {$_SESSION['id']}
-              UNION
-              SELECT usuario_id 
-              FROM amigos 
-              WHERE amigo_id = {$_SESSION['id']}
-          )
-          AND id != {$_SESSION['id']}";
+        AND id NOT IN (
+        SELECT amigo_id 
+        FROM amigos 
+        WHERE usuario_id = {$_SESSION['id']}
+        UNION
+        SELECT usuario_id 
+        FROM amigos 
+        WHERE amigo_id = {$_SESSION['id']}
+        )
+        AND id != {$_SESSION['id']}";
 
 $resultado = $conn->query($sql);
 
 $usuarios = array();
 if ($resultado->num_rows > 0) {
-    while($fila = $resultado->fetch_assoc()) {
+    while ($fila = $resultado->fetch_assoc()) {
         $usuarios[] = $fila;
     }
 }
@@ -33,4 +33,3 @@ header('Content-Type: application/json');
 echo json_encode($usuarios);
 
 $conn->close();
-?>
